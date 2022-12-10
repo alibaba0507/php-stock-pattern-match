e42e9779076182037ee462c282814b4cebe400a5
alibaba0507 <alibaba0507@gmail.com>
2022-12-10 18:54:25 +0200

    init

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+require __DIR__ . "/inc/bootstrap.php";
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+//print_r($uri);
+$indx = array_search("index.php", $uri)-1;
+//echo '----- indx['.$indx.']-------\n';
+if ((!isset($uri[$indx+2]) || !isset($uri[$indx + 3])))
+{
+    header("HTTP/1.1 404 Not Found");
+    exit(); 
+}else if ($uri[$indx+2] == 'rss')
+{
+    if ($uri[$indx + 3] != 'sentiment')
+    {
+        header("HTTP/1.1 404 Not Found");
+        exit();  
+    }
+    require PROJECT_ROOT_PATH . "/Controller/Api/RssController.php";
+    $objFeedController = new RssController();
+    $strMethodName = $uri[$indx+3] . 'Action';
+    $objFeedController->{$strMethodName}();
+}else if ($uri[$indx+2] == 'patterns')
+{
+  //  echo "=----ddddd----------\n";
+    if ($uri[$indx + 3] != 'find'&& $uri[$indx + 3] != 'model')
+    {
+        header("HTTP/1.1 404 Not Found");
+        exit();  
+    }
+   // echo "=--------wwww------\n";
+    require PROJECT_ROOT_PATH . "/Controller/Api/PatternsController.php";
+    $objFeedController = new PatternsController();
+    $strMethodName = $uri[$indx+3] . 'Action';
+    $objFeedController->{$strMethodName}();
+}
+else
+if ((isset($uri[$indx+2]) && $uri[$indx+2] != 'user') || !isset($uri[$indx+3])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}else{
+    require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
+
+    $objFeedController = new UserController();
+    $strMethodName = $uri[3] . 'Action';
+    $objFeedController->{$strMethodName}();
+}
+?>
